d67b39a4ae96470715a07524e7f6ca6900937e60
userChris26 <ch199946@ucf.edu>
2025-07-26 16:03:14 -0400

    Merge conflict resolved

--- mobile/lib/constants/api_constants.dart
+++ mobile/lib/constants/api_constants.dart
@@ -1,12 +1,6 @@
 class ApiConstants {
   // Base URL for API endpoints
-<<<<<<< HEAD
-  // static const String baseUrl = 'https://10.0.2.2:5000';
-  static const String baseUrl = 'https://cop4331iscool.xyz';
-  static const String apiUrl = '$baseUrl/api';
-=======
   static const String apiUrl = 'https://cop4331iscool.xyz/api';
->>>>>>> 01ec5077083af1bb5fae120120788db7b9408785
   
   // API Endpoints
   static const String loginEndpoint = '$apiUrl/login';
